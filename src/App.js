428540921438eef9c354ee1b259e99d42261d344
Zhihao Yang <yang_zhihao@live.com>
2016-11-03 00:11:23 +0000

    Add font in style

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h5>Home</h5>
         </div>
         <div className="App-body">
-          <Row>
+          <Row style={{marginLeft:0, marginRight:0}}>
             <Col xs={0} sm={0} md={1} ld={2}></Col>
             <Col xs={12} sm={12} md={10} ld={8}>
               <div id="about">
@@ -19,7 +19,7 @@ class App extends Component {
                 <h2>I'm Zhihao Yang</h2>
                 <h4>UI/UX Designer & Frontend Developer</h4>
                 <br/>
-                <p>Born and grew up in Sichuan, China, I got my bachelor degree of Software Engineering in East China Normal University(Shanghai, China) in 2015 and is now persuing my master degree at University College London(London, UK).</p> 
+                <p>Born and grew up in Sichuan, China, I got my bachelor degree of Software Engineering at East China Normal University and am now persuing my master degree at University College London.</p> 
                 <p>Previously I have worked in India, China, Switzerland and Sweden as UI/UX Designer and Frontend Developer.</p>
               </div>
             </Col>
