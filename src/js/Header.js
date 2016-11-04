68d82e284b676fe2d05cded8fd9b9fd074dd13dd
Zhihao Yang <yang_zhihao@live.com>
2016-11-04 17:47:04 +0000

    Fix the problem of Nav bar

--- src/js/Header.js
+++ src/js/Header.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Navbar, Nav, NavItem } from 'react-bootstrap';
 import '../css/Header.css'
 import { Link } from 'react-router'
 
 class Header extends Component {
   render() {
     return (
-      <div className="header">
-         <nav className="collapse navbar-collapse" id="myNavbar">
-            <ul className="nav navbar-nav menu">
-              <li><Link to="/home" style={{color: "white"}}>HOME</Link></li>
-              <li><Link to="/resume" style={{color: "white"}}>RESUME</Link></li>
-              <li><Link to="/portfolio" style={{color: "white"}}>PORTFOLIO</Link></li>
-            </ul>
-          </nav>
+      <div id="header">
+        <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem eventKey={1}><Link to="/home" style={{color: "white"}}>HOME</Link></NavItem>
+              <NavItem eventKey={1}><Link to="/resume" style={{color: "white"}}>RESUME</Link></NavItem>
+              <NavItem eventKey={2}><Link to="/portfolio" style={{color: "white"}}>PORTFOLIO</Link></NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </div>
       );
   }
--- src/js/Home.js
+++ src/js/Home.js
@@ -27,7 +27,7 @@ class Home extends Component {
                 <Col xs={12} sm={6} md={6} lg={6}>
                   <Link to="/portfolio" style={{color: "white"}}>
                     <div className="view-portfolio">
-                      <h5>VIEW PORTFOLIO</h5>
+                      <h5>SEE WORKS</h5>
                     </div>
                   </Link>
                 </Col>
@@ -41,22 +41,22 @@ class Home extends Component {
           <div className="section-box">
             <div id="social-media">
               <Row>
-                <Col xs={6} sm={6} md={4} lg={4}>
+                <Col xs={6} sm={6} md={4} lg={2}>
                   <h4><a href="https://www.facebook.com/yangzhihaowill" target="_blank">Facebook</a></h4>
                 </Col>
-                <Col xs={6} sm={6} md={4} lg={4}>
+                <Col xs={6} sm={6} md={4} lg={2}>
                   <h4><a href="https://www.twitter.com/yangzhihao519" target="_blank">Twitter</a></h4>
                 </Col>
-                <Col xs={6} sm={6} md={4} lg={4}>
+                <Col xs={6} sm={6} md={4} lg={2}>
                   <h4><a href="https://www.linkedin.com/in/yangzhihao" target="_blank">Linkedin</a></h4>
                 </Col>
-                <Col xs={6} sm={6} md={4} lg={4}>
+                <Col xs={6} sm={6} md={4} lg={2}>
                   <h4><a href="https://www.instagram.com/yang.z.h" target="_blank">Instagram</a></h4>
                 </Col>
-                <Col xs={6} sm={6} md={4} lg={4}>
+                <Col xs={6} sm={6} md={4} lg={2}>
                   <h4><a href="https://www.github.com/yangzhihao519" target="_blank">Github</a></h4>
                 </Col>
-                <Col xs={6} sm={6} md={4} lg={4}>
+                <Col xs={6} sm={6} md={4} lg={2}>
                   <h4><a href="https://www.behance.net/yangzhihao" target="_blank">Behance</a></h4>
                 </Col>
               </Row>
