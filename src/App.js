b5975d7292063b32d189f3122b6dcd27e3916869
Zhihao Yang <yang_zhihao@live.com>
2016-11-04 11:19:33 +0000

    Create footer

--- src/App.js
+++ src/App.js
@@ -27,6 +27,13 @@ class App extends Component {
           </Row>
         </div>
         <div className="App-footer">
+          <Row style={{marginLeft:0, marginRight:0}}>
+            <Col xs={0} sm={1} md={2} ld={3}></Col>
+            <Col xs={12} sm={10} md={8} ld={6}>
+              <p>© 2016 Hand-crafted by Zhihao Yang.&nbsp; All Rights Reserved</p>
+            </Col>
+            <Col xs={0} sm={1} md={2} ld={3}></Col>
+          </Row>
         </div>
       </div>
       );
