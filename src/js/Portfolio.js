370ac6d3845eeed27603f457754b6d8314c54c28
Zhihao Yang <yang_zhihao@live.com>
2016-11-07 23:28:59 +0000

    Add style to portfolio page to show hover content

--- src/js/Portfolio.js
+++ src/js/Portfolio.js
@@ -18,64 +18,88 @@ class Portfolio extends Component {
 
                 <Row>
                     <Col xs={12} sm={6} md={6} lg={6}>
-                        <div className="portfolio-item" >
-                            <div className="portfolio-item-image">
-                                <img src={h2020_screenshot} />
+                        <a href="#">
+                            <div className="portfolio-item" >
+                                <div className="portfolio-item-image">
+                                    <img src={h2020_screenshot} />
+                                </div>
+                                <div className="portfolio-item-title">
+                                    <h4 style={{ fontWeight: 700 }}>Web-based Visualisation Tool of Horizon 2020</h4>
+                                </div>
+                                <div className="portfolio-item-content">
+                                    <p>Build a web-based visualisation tool of Horizon 2020 at the Swedish Innovation Agency.</p>
+                                    <p>Work mainly focuses on UI/UX design and front-end development.</p>
+                                    <a href="#">
+                                        <div className="view-more-button">
+                                            <p style={{ marginBottom: 0 + "px" }}>View More</p>
+                                        </div>
+                                    </a>
+                                </div>
                             </div>
-                            <div className="portfolio-item-title">
-                                <h3>Web-based Visualisation Tool of Horizon 2020</h3>
-                            </div>
-                            <div className="portfolio-item-content">
-                                <h4>2016.06-2016.09</h4>
-                                <p>Build a web-based visualisation tool of Horizon 2020 at the Swedish Innovation Agency.</p>
-                                <p>Work mainly focuses on UI/UX design and front-end development.</p>
-                            </div>
-                        </div>
+                        </a>
                     </Col>
                     <Col xs={12} sm={6} md={6} lg={6}>
-                        <div className="portfolio-item" >
-                            <div className="portfolio-item-image">
-                                <img src={saw_screenshot} />
+                        <a href="#">
+                            <div className="portfolio-item" >
+                                <div className="portfolio-item-image">
+                                    <img src={saw_screenshot} />
+                                </div>
+                                <div className="portfolio-item-title">
+                                    <h4 style={{ fontWeight: 700 }}>Interaction Design of Seizure Alert Watch & APP</h4>
+                                </div>
+                                <div className="portfolio-item-content">
+                                    <p>Design a Assistive Technology to support the epileptic patients and help them live as normal people;</p>
+                                    <p>Work mainly focuses on ideation, user research, UI/UX of low-fi and high-fi prototype of the applications.</p>
+                                    <a href="#">
+                                        <div className="view-more-button">
+                                            <p style={{ marginBottom: 0 + "px" }}>View More</p>
+                                        </div>
+                                    </a>
+                                </div>
                             </div>
-                            <div className="portfolio-item-title">
-                                <h3>Interaction Design of Seizure Alert Watch & APP</h3>
-                            </div>
-                            <div className="portfolio-item-content">
-                                <h4>2015.11–2016.01</h4>
-                                <p>Design a Assistive Technology to support the epileptic patients and help them live as normal people;</p>
-                                <p>Work mainly focuses on ideation, user research, UI/UX of low-fi and high-fi prototype of the applications.</p>
-                            </div>
-                        </div>
+                        </a>
                     </Col>
                     <Col xs={12} sm={6} md={6} lg={6}>
-                        <div className="portfolio-item" >
-                            <div className="portfolio-item-image">
-                                <img src={yorktail_screenshot} />
+                        <a href="#">
+                            <div className="portfolio-item" >
+                                <div className="portfolio-item-image">
+                                    <img src={yorktail_screenshot} />
+                                </div>
+                                <div className="portfolio-item-title">
+                                    <h4 style={{ fontWeight: 700 }}>Yocktail - Great Platform for Cocktail Lovers</h4>
+                                </div>
+                                <div className="portfolio-item-content">
+                                    <p>Yocktail is a platform offered to cocktail lovers, allowing users to search, collect, create and share cocktails;</p>
+                                    <p>Work as a full-stack designer and developer, from UI/UX design, to front-end & back-end programming.</p>
+                                    <a href="#">
+                                        <div className="view-more-button">
+                                            <p style={{ marginBottom: 0 + "px" }}>View More</p>
+                                        </div>
+                                    </a>
+                                </div>
                             </div>
-                            <div className="portfolio-item-title">
-                                <h3>Yocktail - Great Platform for Cocktail Lovers</h3>
-                            </div>
-                            <div className="portfolio-item-content">
-                                <h4>2016.03–2016.04</h4>
-                                <p>Yocktail is a platform offered to cocktail lovers, allowing users to search, collect, create and share cocktails;</p>
-                                <p>Work as a full-stack designer and developer, from UI/UX design, to front-end & back-end programming.</p>
-                            </div>
-                        </div>
+                        </a>
                     </Col>
                     <Col xs={12} sm={6} md={6} lg={6}>
-                        <div className="portfolio-item" >
-                            <div className="portfolio-item-image">
-                                <img src={vinyo_screenshot} />
+                        <a href="#">
+                            <div className="portfolio-item" >
+                                <div className="portfolio-item-image">
+                                    <img src={vinyo_screenshot} />
+                                </div>
+                                <div className="portfolio-item-title">
+                                    <h4 style={{ fontWeight: 700 }}>Vinyo: Buy the wine you love on mobile</h4>
+                                </div>
+                                <div className="portfolio-item-content">
+                                    <p>Vinyo APP is a critically-acclaimed, free app that helps you search, find and buy the wine you want. </p>
+                                    <p>Work focuses on the client-end development of this iOS application.</p>
+                                    <a href="#">
+                                        <div className="view-more-button">
+                                            <p style={{ marginBottom: 0 + "px" }}>View More</p>
+                                        </div>
+                                    </a>
+                                </div>
                             </div>
-                            <div className="portfolio-item-title">
-                                <h3>Vinyo: Buy the wine you love on mobile</h3>
-                            </div>
-                            <div className="portfolio-item-content">
-                                <h4>2014.06–2015.01</h4>
-                                <p>Vinyo APP is a critically-acclaimed, free app that helps you search, find and buy the wine you want. </p>
-                                <p>Work focuses on the client-end development of this iOS application.</p>
-                            </div>
-                        </div>
+                        </a>
                     </Col>
                 </Row>
             </div>
