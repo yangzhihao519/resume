627e6d739dfada924a5e4b895ec9a4bae5d73a4e
Zhihao Yang <yang_zhihao@live.com>
2016-11-14 22:49:19 +0000

    create component PortfolioItemPageSection and update css styles

--- src/js/PortfolioItemPage.js
+++ src/js/PortfolioItemPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import '../css/App.css'
 import '../css/Portfolio.css'
 import { Row, Col } from 'react-bootstrap'
+import {PortfolioItemPageSection} from './PortfolioItemPageSection'
 
 var portfolioItems = require('../data/PortfolioItems.json');
 
@@ -15,8 +16,11 @@ class PortfolioItemPage extends Component {
         const id = this.props.params.id;
         const portfolioItem = portfolioItems[id - 1];
         const title = portfolioItem.title;
-        var coverImageName = portfolioItem.cover_image_name;
-        var periodOfTime = portfolioItem.period_of_time;
+        const coverImageName = portfolioItem.cover_image_name;
+        const periodOfTime = portfolioItem.period_of_time;
+        const location = portfolioItem.location;
+        const teamMembers = portfolioItem.team_members;
+        const skills = portfolioItem.skills;
 
         var descriptionsHTML = [];
         var descriptions = portfolioItem.descriptions;
@@ -25,22 +29,66 @@ class PortfolioItemPage extends Component {
             descriptionsHTML.push(<h4 key={description}>{description}</h4>);
         }
 
+        const problem = portfolioItem.problem;
+        const solution = portfolioItem.solution;
+        const process = portfolioItem.result;
+        const result = portfolioItem.result
+
         return (
             <div id="portfolio-item-page">
-                <div id="route">
-                    <a href="#/portfolio"><h5>Back</h5></a>
+                <div className="portfolio-item-page-title">
+                    <h2>{title}</h2>
                 </div>
-                <div id="portfolio-item-page-content">
+                <div id="portfolio-item-page-intro">
                     <div id="portfolio-item-page-cover-image">
                         <img src={require('../images/' + coverImageName)} />
-                    </div>
-                    <div id="portfolio-item-page-title">
-                        <h2>{title}</h2>
-                        <h4 style={{ fontWeight: 300 }}>{periodOfTime}</h4>
                     </div>
                     <div id="portfolio-item-page-descriptions">
                         {descriptionsHTML}
                     </div>
+                    <div id="portfolio-item-page-info">
+                        <Row>
+                            <Col xs={4} sm={4} md={4} lg={3} className="title">
+                                <h4>Time</h4>
+                            </Col>
+                            <Col xs={8} sm={8} md={8} lg={9} className="content">
+                                <h4>{periodOfTime}</h4>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={4} sm={4} md={4} lg={3} className="title">
+                                <h4>Location</h4>
+                            </Col>
+                            <Col xs={8} sm={8} md={8} lg={9} className="content">
+                                <h4>{location}</h4>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={4} sm={4} md={4} lg={3} className="title">
+                                <h4>Team members</h4>
+                            </Col>
+                            <Col xs={8} sm={8} md={8} lg={9} className="content">
+                                <h4>{teamMembers}</h4>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={4} sm={4} md={4} lg={3} className="title">
+                                <h4>Skills Used</h4>
+                            </Col>
+                            <Col xs={8} sm={8} md={8} lg={9} className="content">
+                                <h4>{skills}</h4>
+                            </Col>
+                        </Row>
+                    </div>
+                </div>
+                <div className="portfolio-item-page-title">
+                    <h3>The Story</h3>
+                </div>
+                <div id="portfolio-item-page-story">
+                    {problem === "" ? "" : <PortfolioItemPageSection title={"Problem"} content={problem}/>}
+                        {solution === "" ? "" : <PortfolioItemPageSection title={"Solution"} content={solution}/>}
+                        {process === "" ? "" : <PortfolioItemPageSection title={"Process"} content={process}/>}
+                        {result === "" ? "" : <PortfolioItemPageSection title={"Result"} content={result}/>}
                 </div>
             </div>
         );
---
+++ src/js/PortfolioItemPageSection.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import '../css/App.css'
+import '../css/Portfolio.css'
+
+interface PortfolioItemPageSectionProps {
+    title: string;
+    content: {
+        images: [],
+        text: string
+    }
+}
+
+class PortfolioItemPageSection extends Component<PortfolioItemPageSectionProps, {}> {
+
+    render() {
+        const title = this.props.title;
+        const text = this.props.content.text;
+
+        var imagesHTML = [];
+        var images = this.props.content.images;
+        for (var i = 0; i < images.length; i++) {
+            var image = images[i];
+            imagesHTML.push(<img src={require('../images/' + image)} />);
+            imagesHTML.push(<br />);
+        }
+
+        return (
+            <div className="portfolio-item-page-section">
+                <div className="title">
+                    <h3 style={{fontWeight: 700}}>{title}</h3>
+                </div>
+                <div className="content">
+                    <div className="text">
+                        <h4>{text}</h4>
+                    </div>
+                    <div className="images">
+                        {imagesHTML}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export { PortfolioItemPageSection };
