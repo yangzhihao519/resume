de7b9fb4bb1494880524beaf1eff4fa6d4492817
Zhihao Yang <yang_zhihao@live.com>
2017-03-02 21:05:39 +0000

    Homepage edits

--- src/js/Home.js
+++ src/js/Home.js
@@ -11,7 +11,7 @@ class Home extends Component {
         <div className="section-box" id="section-about">
           <h3 id="hello">Hello!</h3>
           <h2><span>I'm</span>&nbsp; Zhihao Yang</h2>
-          <h4>UI/UX Designer & Front-end Developer</h4>
+          <h4>UX Designer & Front-end Developer</h4>
           <br />
           <p>Born and grew up in Sichuan, China, I got my bachelor degree of Software Engineering at East China Normal University and am now pursuing my master degree at University College London in Human Computer Interaction and Design.</p>
           <p>Previously I have worked at university, startups and governmental agency in India, China, Switzerland and Sweden as UI/UX Designer and Frontend Developer.</p>
@@ -53,7 +53,7 @@ class Home extends Component {
                     <img src={require('../images/portfolio/h2020_cover.png')} role="presentation"/>
                     <div className="portfolio-item-content">
                       <h4>Web-based Visualisation Tool of Horizon 2020</h4>
-                      <p>Work mainly focuses on UI/UX design and front-end development.</p>
+                      <p>Work mainly focuses on UI & UX design and front-end development.</p>
                                 <div className="view-more-button">
                         <p style={{ marginBottom: 0 + "px" }}>View More</p>
                       </div>
@@ -69,7 +69,7 @@ class Home extends Component {
                     <img src={require('../images/portfolio/saw_cover.png')} role="presentation"/>
                     <div className="portfolio-item-content">
                       <h4>Interaction Design of Seizure Alert Watch & APP</h4>
-                      <p>Work mainly focuses on ideation, user research, UI/UX of low-fi and high-fi prototype of the applications.</p>
+                      <p>Work mainly focuses on ideation, user research, UI & UX of low-fi and high-fi prototype of the applications.</p>
                       <div className="view-more-button">
                         <p style={{ marginBottom: 0 + "px" }}>View More</p>
                       </div>
@@ -90,35 +90,31 @@ class Home extends Component {
             <h4 style={{fontWeight: 300}}>Let me know if you have any interesting ideas/projects to work on!</h4>
             <a href="mailto:yang_zhihao@live.com" id="email-box"><h5>SEND ME AN EMAIL</h5></a>
           </div>
-        </div>
-
-        <div className="section-title">
-          <h3>Find me somewhere else</h3>
-        </div>
-        <div className="section-box">
           <div id="social-media">
             <Row>
-              <Col xs={6} sm={6} md={4} lg={2}>
-                <h4><a href="https://www.facebook.com/yangzhihaowill" target="_blank">Facebook</a></h4>
+              <Col xs={0} sm={0} md={0} lg={2}>
+                
               </Col>
-              <Col xs={6} sm={6} md={4} lg={2}>
+              <Col xs={6} sm={6} md={3} lg={2}>
                 <h4><a href="https://www.twitter.com/yangzhihao519" target="_blank">Twitter</a></h4>
               </Col>
-              <Col xs={6} sm={6} md={4} lg={2}>
+              <Col xs={6} sm={6} md={3} lg={2}>
                 <h4><a href="https://www.linkedin.com/in/yangzhihao" target="_blank">Linkedin</a></h4>
               </Col>
-              <Col xs={6} sm={6} md={4} lg={2}>
-                <h4><a href="https://www.instagram.com/yang.z.h" target="_blank">Instagram</a></h4>
-              </Col>
-              <Col xs={6} sm={6} md={4} lg={2}>
+              
+              <Col xs={6} sm={6} md={3} lg={2}>
                 <h4><a href="https://www.github.com/yangzhihao519" target="_blank">Github</a></h4>
               </Col>
-              <Col xs={6} sm={6} md={4} lg={2}>
+              <Col xs={6} sm={6} md={3} lg={2}>
                 <h4><a href="https://www.behance.net/yangzhihao" target="_blank">Behance</a></h4>
+              </Col>
+              <Col xs={0} sm={0} md={0} lg={2}>
+                
               </Col>
             </Row>
           </div>
         </div>
+
       </div>
     );
   }
