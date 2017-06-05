c2edc7ba1e1f58a508f0264f9aeb1a32287e6608
Zhihao Yang <yang_zhihao@live.com>
2017-06-05 17:09:46 +0100

    Add work on the homepage

--- src/js/Home.js
+++ src/js/Home.js
@@ -42,10 +42,10 @@ class Home extends Component {
             <Row style={{marginLeft:0, marginRight:0}}>
               <Col xs={0} sm={1} md={1} lg={2}></Col>
               <Col xs={12} sm={10} md={10} lg={8}>
-                  <h3>Zhihao is fascinated about using digital solution to change the way we are living using the skills he has: high creativity and efficiency in sketching out prototypes and solid skills in front-end development to bring pure ideas into life.</h3>
+                  <h3>Fascinated about using technology to change the way we are living, Zhihao is eager to contribute to the digital transformation era with his high creativity and efficiency in sketching out prototypes and solid skills in front-end development to bring pure ideas into life.</h3>
                   <Link to="/portfolio" style={{ color: "white" }}>
                     <div id="download-cv">
-                      <h5><a href="https://drive.google.com/file/d/0B209VNQwXS5QNFhvRmtDUER1Uzg/view?usp=sharing" target="_blank">Download CV <span className="glyphicon glyphicon-download-alt"></span></a></h5>
+                      <h4><a href="https://drive.google.com/file/d/0B209VNQwXS5QNFhvRmtDUER1Uzg/view?usp=sharing" target="_blank">Download CV </a></h4>
                     </div>
                   </Link>
               </Col>
@@ -77,6 +77,106 @@ class Home extends Component {
                 <Col xs={0} sm={0} md={1} lg={1}></Col>
             </Row>
           </div>
+
+          <div className="work-item" id="work-item-2">
+            <Row style={{marginLeft:0, marginRight:0}}>
+                <Col xs={0} sm={0} md={1} lg={1}></Col>
+                <Col xs={12} sm={6} md={5} lg={5}>
+                    <div className="work-item-image">
+                        <img src={require('../images/portfolio/saw_cover.png')} role="presentation"/>
+                    </div>
+                </Col>
+                <Col xs={0} sm={0} md={1} lg={1}>
+                </Col>
+                <Col xs={12} sm={6} md={4} lg={4}>
+                    <h3>Interaction Design</h3>
+                    <h1>Seizure Alert Watch & APP</h1>
+                    <h4>Designing an Assistive Technology to support the epileptic patients and help them live as normal people.</h4>
+                    <a href="#/portfolio/2" target="_blank">
+                       <div className="view-more-button">
+                          <h4 style={{fontWeight: 600, margin: 0}}>View more</h4>
+                       </div>
+                    </a>
+                </Col>
+                <Col xs={0} sm={0} md={1} lg={1}></Col>
+            </Row>
+          </div>
+
+          <div className="work-item" id="work-item-3">
+            <Row style={{marginLeft:0, marginRight:0}}>
+                <Col xs={0} sm={0} md={1} lg={1}></Col>
+                <Col xs={12} sm={6} md={4} lg={4}>
+                    <h3>iOS APP Development</h3>
+                    <h1>Vinyo: Buy the wine you love on mobile</h1>
+                    <h4>Vinyo APP is a critically-acclaimed, free app that helps you search, find and buy the wine you want</h4>
+                    <a href="#/portfolio/5" target="_blank">
+                       <div className="view-more-button">
+                          <h4 style={{fontWeight: 600, margin: 0}}>View more</h4>
+                       </div>
+                    </a>
+                </Col>
+                <Col xs={0} sm={0} md={1} lg={1}>
+                </Col>
+                <Col xs={12} sm={6} md={5} lg={5}>
+                    <div className="work-item-image">
+                        <img src={require('../images/portfolio/vinyo_cover.png')} role="presentation"/>
+                    </div>
+                </Col>
+                <Col xs={0} sm={0} md={1} lg={1}></Col>
+            </Row>
+          </div>
+
+          <div className="work-item" id="work-item-4">
+            <Row style={{marginLeft:0, marginRight:0}}>
+                <Col xs={0} sm={0} md={1} lg={1}></Col>
+                <Col xs={12} sm={6} md={5} lg={5}>
+                    <div className="work-item-image">
+                        <img src={require('../images/portfolio/yorktail_cover.png')} role="presentation"/>
+                    </div>
+                </Col>
+                <Col xs={0} sm={0} md={1} lg={1}>
+                </Col>
+                <Col xs={12} sm={6} md={4} lg={4}>
+                    <h3>Full stack</h3>
+                    <h1>Yocktail: Great Platform for Cocktail Lovers</h1>
+                    <h4>Yocktail is a platform offered to cocktail lovers, allowing users to search, collect, create and share cocktails.</h4>
+                    <a href="#/portfolio/4" target="_blank">
+                       <div className="view-more-button">
+                          <h4 style={{fontWeight: 600, margin: 0}}>View more</h4>
+                       </div>
+                    </a>
+                </Col>
+                <Col xs={0} sm={0} md={1} lg={1}></Col>
+            </Row>
+          </div>
+
+          <div className="work-item" id="work-item-5">
+            <Row style={{marginLeft:0, marginRight:0}}>
+                <Col xs={0} sm={0} md={1} lg={1}></Col>
+                <Col xs={12} sm={6} md={4} lg={4}>
+                    <h3>UI Design</h3>
+                    <h1>Tellus News: keep up to date</h1>
+                    <h4>Developing a technology to summarise and analyse news from all over the world.</h4>
+                    <a href="#/portfolio/3" target="_blank">
+                       <div className="view-more-button">
+                          <h4 style={{fontWeight: 600, margin: 0}}>View more</h4>
+                       </div>
+                    </a>
+                </Col>
+                <Col xs={0} sm={0} md={1} lg={1}>
+                </Col>
+                <Col xs={12} sm={6} md={5} lg={5}>
+                    <div className="work-item-image">
+                        <img src={require('../images/portfolio/tellus_cover.png')} role="presentation"/>
+                    </div>
+                </Col>
+                <Col xs={0} sm={0} md={1} lg={1}></Col>
+            </Row>
+          </div>
+        </div>
+
+        <div id="references">
+            
         </div>
 
         <div id="contact">
