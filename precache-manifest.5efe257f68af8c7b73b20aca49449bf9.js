bd41764bc7f6d95f6974b85c0311386e0661e6b2
Zhihao Yang <yang_zhihao@live.com>
2020-03-15 23:26:40 +0000

    Updates

---
+++ precache-manifest.5efe257f68af8c7b73b20aca49449bf9.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f7812ade808a0a48635c58a1c6c317e",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "94edbc4e6f7e537df68b",
+    "url": "/site/static/css/2.9ddb28ea.chunk.css"
+  },
+  {
+    "revision": "2ceb7ca26694fb851c6b",
+    "url": "/site/static/css/main.1072e9f2.chunk.css"
+  },
+  {
+    "revision": "94edbc4e6f7e537df68b",
+    "url": "/site/static/js/2.fc86c889.chunk.js"
+  },
+  {
+    "revision": "b950b862c86b4cccabea4c9acacc23df",
+    "url": "/site/static/js/2.fc86c889.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ceb7ca26694fb851c6b",
+    "url": "/site/static/js/main.71ea0b59.chunk.js"
+  },
+  {
+    "revision": "53ed87dec1d8ebba81c6",
+    "url": "/site/static/js/runtime-main.a07538af.js"
+  },
+  {
+    "revision": "6fee896258544fb824e60c93a3e5b697",
+    "url": "/site/static/media/1.6fee8962.md"
+  },
+  {
+    "revision": "416d429f4519a1cfbfdd1f05916fe24d",
+    "url": "/site/static/media/2.416d429f.md"
+  },
+  {
+    "revision": "21f979c7aa34eaf161a79a754fd959c1",
+    "url": "/site/static/media/3.21f979c7.md"
+  },
+  {
+    "revision": "f3724d41dc165cfc44a06bd7bf1d395a",
+    "url": "/site/static/media/4.f3724d41.md"
+  },
+  {
+    "revision": "a36178be09d67cb6606f84324d74c4f5",
+    "url": "/site/static/media/5.a36178be.md"
+  },
+  {
+    "revision": "45d08807b2d81a46394bab057339384c",
+    "url": "/site/static/media/h2020_cover.45d08807.png"
+  },
+  {
+    "revision": "c696d1d719fda0e04e959e975020cabc",
+    "url": "/site/static/media/h2020_process_evaluation.c696d1d7.png"
+  },
+  {
+    "revision": "4e70ce032ad1b7239372768b801bacbc",
+    "url": "/site/static/media/h2020_process_highfi_prototype.4e70ce03.png"
+  },
+  {
+    "revision": "60af25699549668a137078de2ecb75e2",
+    "url": "/site/static/media/h2020_process_implementation.60af2569.png"
+  },
+  {
+    "revision": "ef4621557b66ddf34fd5dc49d837eba2",
+    "url": "/site/static/media/h2020_process_midfi_prototype.ef462155.png"
+  },
+  {
+    "revision": "672378ceb94c6f5e9903ade2cf24a9f2",
+    "url": "/site/static/media/h2020_process_sketch.672378ce.png"
+  },
+  {
+    "revision": "5b51cdb0fad1d5c8140f617696d9dc98",
+    "url": "/site/static/media/h2020_process_weekly_meetings.5b51cdb0.png"
+  },
+  {
+    "revision": "b1d49e84155df61c4bbd1a544ac1f66f",
+    "url": "/site/static/media/h2020_results_conference.b1d49e84.png"
+  },
+  {
+    "revision": "eaddd2f0323f11e75dd6f05b713ed0ef",
+    "url": "/site/static/media/h2020_results_final.eaddd2f0.png"
+  },
+  {
+    "revision": "c5f9459c9223e30b84d69146261039e1",
+    "url": "/site/static/media/h2020_solution_data_structure.c5f9459c.png"
+  },
+  {
+    "revision": "8c67f71a357b48b85c17f23cf5e112ef",
+    "url": "/site/static/media/h2020_solution_data_visualisation.8c67f71a.png"
+  },
+  {
+    "revision": "eab8058cfd715830c60083b396da8993",
+    "url": "/site/static/media/saw_cover.eab8058c.png"
+  },
+  {
+    "revision": "da62d8502f4b5dbb39bc0fffbb08bbcf",
+    "url": "/site/static/media/saw_process_brainstorming.da62d850.png"
+  },
+  {
+    "revision": "e889ade96805802edf9b5657c143f173",
+    "url": "/site/static/media/saw_process_persona.e889ade9.png"
+  },
+  {
+    "revision": "e7df9007bf0b4d0f20c18d0c047a3155",
+    "url": "/site/static/media/saw_process_pop.e7df9007.png"
+  },
+  {
+    "revision": "11399dcbb07567db6be291998ca7330e",
+    "url": "/site/static/media/saw_process_sketch.11399dcb.png"
+  },
+  {
+    "revision": "8a523225b0916c7be8a24700c482b7f2",
+    "url": "/site/static/media/saw_result_smartphone.8a523225.png"
+  },
+  {
+    "revision": "fd0268e6e6ecb43a0f72c879f241773c",
+    "url": "/site/static/media/saw_result_smartwatch.fd0268e6.png"
+  },
+  {
+    "revision": "d2f02ecfb2b00a3681f2986b59d6168b",
+    "url": "/site/static/media/tellus_cover.d2f02ecf.png"
+  },
+  {
+    "revision": "f8baa67c5084461037b67e6098e47669",
+    "url": "/site/static/media/tellus_dashboard_1.f8baa67c.png"
+  },
+  {
+    "revision": "83bcf14c7c4b3c3e19659869db696e2b",
+    "url": "/site/static/media/tellus_dashboard_2.83bcf14c.png"
+  },
+  {
+    "revision": "587235c9835b1bd9424efcf0d526ff30",
+    "url": "/site/static/media/tellus_factbox_ui_1.587235c9.png"
+  },
+  {
+    "revision": "dccbcc97e4c9e6ed3012a225b68ebace",
+    "url": "/site/static/media/tellus_factbox_ui_2.dccbcc97.png"
+  },
+  {
+    "revision": "ab322f67994d98347a859457f3a7bf9f",
+    "url": "/site/static/media/tellus_factbox_ui_3.ab322f67.png"
+  },
+  {
+    "revision": "765a2d23b74892f1a73ce1ab83ad3cda",
+    "url": "/site/static/media/tellus_factbox_ui_4.765a2d23.png"
+  },
+  {
+    "revision": "79218525bd39921d0b1b703dfa9f2168",
+    "url": "/site/static/media/tellus_redesign_ui_1.79218525.png"
+  },
+  {
+    "revision": "3a08964df0ddef7839e0cc057fd96a46",
+    "url": "/site/static/media/tellus_redesign_ui_2.3a08964d.png"
+  },
+  {
+    "revision": "d91dd2c0288b6cfc220cfe2bb6a6ca1e",
+    "url": "/site/static/media/tellus_redesign_ui_3.d91dd2c0.png"
+  },
+  {
+    "revision": "bb5f0bec4f5310729a38b99ec2c4482e",
+    "url": "/site/static/media/vinyo_app_screenshot.bb5f0bec.png"
+  },
+  {
+    "revision": "61e269a2f83ae2cb1e2893282d4ce228",
+    "url": "/site/static/media/vinyo_cover.61e269a2.png"
+  },
+  {
+    "revision": "93e106c64740baf937745ac1c0e8aa11",
+    "url": "/site/static/media/vinyo_website_screenshot.93e106c6.png"
+  },
+  {
+    "revision": "e2e563d19e578388b220fcbd9e10c9cc",
+    "url": "/site/static/media/yocktail_process_firebase.e2e563d1.png"
+  },
+  {
+    "revision": "d106846acaa998ae1c3da8c20d18f4bb",
+    "url": "/site/static/media/yocktail_process_hifi_about.d106846a.png"
+  },
+  {
+    "revision": "d12bc5dec3813a50e9d099b45e981e60",
+    "url": "/site/static/media/yocktail_process_hifi_cocktail.d12bc5de.png"
+  },
+  {
+    "revision": "a2772f3fc44dcba1f75c71b874faf036",
+    "url": "/site/static/media/yocktail_process_hifi_community.a2772f3f.png"
+  },
+  {
+    "revision": "bb0d832d6f3c25c9ca358f67f4e5bee0",
+    "url": "/site/static/media/yocktail_process_hifi_create_cocktail.bb0d832d.png"
+  },
+  {
+    "revision": "2e34712127ea1ac3bd6f77ac00bb0f80",
+    "url": "/site/static/media/yocktail_process_hifi_explore.2e347121.png"
+  },
+  {
+    "revision": "702aa4fd9b8e0f192a54065162f772f5",
+    "url": "/site/static/media/yocktail_process_hifi_home.702aa4fd.png"
+  },
+  {
+    "revision": "37cfb49cc4a08758ae05afcdc695957e",
+    "url": "/site/static/media/yocktail_process_hifi_profile.37cfb49c.png"
+  },
+  {
+    "revision": "feed6fa54c5b206424eff99cc5e8b8ae",
+    "url": "/site/static/media/yocktail_process_hifi_signin.feed6fa5.png"
+  },
+  {
+    "revision": "e5ca828f710f9bf9318e9a75007d028d",
+    "url": "/site/static/media/yocktail_process_hifi_signup.e5ca828f.png"
+  },
+  {
+    "revision": "32656af5257bb59e91d8e9ad81645bf6",
+    "url": "/site/static/media/yocktail_results_multi_devices.32656af5.png"
+  },
+  {
+    "revision": "0858cf9ba7fa478a87e88c053f60ad64",
+    "url": "/site/static/media/yorktail_cover.0858cf9b.png"
+  }
+]);
